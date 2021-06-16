5b09092e50ebb34282f259ca8b3cf5e6a845b352
Michael Heap <m@michaelheap.com>
2021-06-16 14:28:10 +0100

    Always fetch commit SHA
    This fixes an issue where a tag SHA used to be accepted by the GH Actions runner but is no longer working.
    
    The documentation has always specified _commit_ hash, and this commit adds support for looking up a commit SHA when provided with a tag ref

--- findRefOnGithub.js
+++ findRefOnGithub.js
@@ -9,27 +9,50 @@ module.exports = function(action) {
     const repo = action.repo;
     const pinned = action.pinnedVersion;
 
-    let sha = null;
-
-    // In order: SHA, Tag, Branch
-    const possibleRefs = [pinned, `tags/${pinned}`, `heads/${pinned}`];
+    // In order: Tag, Branch
+    const possibleRefs = [`tags/${pinned}`, `heads/${pinned}`];
     for (let ref of possibleRefs) {
       try {
-        sha = (
+        const object = (
           await github.git.getRef({
             owner,
             repo,
             ref
           })
-        ).data.object.sha;
+        ).data.object;
 
-        if (sha) {
-          return resolve(sha);
+        // If it's a tag, fetch the commit hash instead
+        if (object.type === "tag") {
+          // Fetch the commit hash instead
+          const tag = await github.git.getTag({
+            owner,
+            repo,
+            tag_sha: object.sha
+          });
+          return resolve(tag.data.object.sha);
+        }
+
+        // If it's already a commit, return that
+        if (object.type === "commit") {
+          return resolve(object.sha);
         }
       } catch (e) {
         // We can ignore failures as we validate later
         //console.log(e);
       }
+    }
+
+    // If we get this far, have we been provided with a specific commit SHA?
+    try {
+      const commit = await github.repos.getCommit({
+        owner,
+        repo,
+        ref: pinned
+      });
+      return resolve(commit.data.sha);
+    } catch (e) {
+      // If it's not a commit, it doesn't matter
+      //console.log(e);
     }
 
     return reject(
