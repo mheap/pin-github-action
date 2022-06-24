db160fade0011f53b24b7b0513319b38b0b5b8f2
Lucas Gonze <lucas@gonze.com>
2022-06-23 19:32:50 -0700

    feat: multiple input files
    Signed-off-by: Lucas Gonze <lucas@gonze.com>

--- bin.js
+++ bin.js
@@ -37,9 +37,7 @@ const packageDetails = require(path.join(__dirname, "package.json"));
       )
       .parse(process.argv);
 
-    const filename = program.args[0];
-
-    if (!filename) {
+    if ( program.args.length == 0 ){
       console.log("Usage: pin-github-action /path/to/workflow.yml");
       process.exit(1);
     }
@@ -51,19 +49,25 @@ const packageDetails = require(path.join(__dirname, "package.json"));
     let yamlLineWidth = program.opts().yamlLineWidth;
     let yamlNullStr = program.opts().yamlNullStr;
 
-    const input = fs.readFileSync(filename).toString();
+    for( const filename of program.args ){
+      if (!fs.existsSync(filename)) {
+        throw("No such file: "+filename);
+      }
+    }
 
-    const output = await run(
-      input,
-      allowed,
-      ignoreShas,
-      allowEmpty,
-      debug,
-      yamlLineWidth,
-      yamlNullStr
-    );
-
-    fs.writeFileSync(filename, output.workflow);
+    for( const filename of program.args ){
+      const input = fs.readFileSync(filename).toString();
+      const output = await run( 
+        input,
+        allowed,
+        ignoreShas,
+        allowEmpty,
+        debug,
+        yamlLineWidth,
+        yamlNullStr 
+      );
+      fs.writeFileSync(filename, output.workflow);
+    }
 
     // Once run on a schedule, have it return a list of changes, along with SHA links
     // and generate a PR to update the actions to the latest version. This allows them a
