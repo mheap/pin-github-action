e0c41f038a7d982f250aff530caecb8002a466c0
Pravin Dahal <mail@pravindahal.com>
2025-02-14 18:06:48 +0100

    fix: Add support for quoted uses values (#184)

--- replaceActions.js
+++ replaceActions.js
@@ -6,30 +6,33 @@ export default function (input, action, comment) {
   const actionId = `${owner}/${repo}${path ? `/${path}` : ""}`;
   const newComment = generateComment(action, comment);
 
+  // Capture an optional quote (either `"` or `'`)
+  const quotePattern = "([\"'])?";
+
   const regexpUpdate = new RegExp(
-    `uses(\\s*):(\\s+)${escapeStringRegexp(actionId)}@${escapeStringRegexp(
-      currentVersion
-    )}(\\s*)#[^\\n]*`,
+    `uses(\\s*):(\\s+)${quotePattern}${escapeStringRegexp(
+      actionId
+    )}@${escapeStringRegexp(currentVersion)}\\3(\\s*)#[^\\n]*`,
     "g"
   );
 
   if (regexpUpdate.test(input)) {
     return input.replace(
       regexpUpdate,
-      `uses$1:$2${actionId}@${newVersion}$3#${newComment}`
+      `uses$1:$2$3${actionId}@${newVersion}$3$4#${newComment}`
     );
   }
 
   const regexpPin = new RegExp(
-    `uses(\\s*):(\\s+)${escapeStringRegexp(actionId)}@${escapeStringRegexp(
-      currentVersion
-    )}`,
+    `uses(\\s*):(\\s+)${quotePattern}${escapeStringRegexp(
+      actionId
+    )}@${escapeStringRegexp(currentVersion)}\\3`,
     "g"
   );
 
   return input.replace(
     regexpPin,
-    `uses$1:$2${actionId}@${newVersion} #${newComment}`
+    `uses$1:$2$3${actionId}@${newVersion}$3 #${newComment}`
   );
 }
 
