0b987754cfa351ff0379bd221af5fdd3e5995378
Michael Heap <m@michaelheap.com>
2025-04-27 13:39:38 +0100

    Add support for --recursive flag

--- collectWorkflowFiles.test.js
+++ collectWorkflowFiles.test.js
@@ -20,6 +20,17 @@ test("collects all files from given path", () => {
   ]);
 });
 
+test("collects files recursively if needed", () => {
+  const programArgs = ["__fixtures__"];
+
+  expect(collectWorkflowFiles(programArgs, true)).toEqual([
+    "__fixtures__/composite.yml",
+    "__fixtures__/reusable.yml",
+    "__fixtures__/workflow.yml",
+    "__fixtures__/recursive/works.yml",
+  ]);
+});
+
 test("collects single file from given path", () => {
   const programArgs = ["__fixtures__/composite.yml"];
 
